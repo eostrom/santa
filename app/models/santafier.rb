29de43c84017c6d785fc117b3ab69e657e935788
Erik Ostrom <erik@echographia.com>
2013-12-01 14:08:58 -0600

    Do santafy algorithm in memory.
    It's just less confusing, and the performance impact for a data set
    this small is moot and/or positive.

--- app/models/santafier.rb
+++ app/models/santafier.rb
@@ -1,17 +1,32 @@
 class Santafier
   def initialize(people)
     @people = people
+
+    @recipients = people.to_a.dup
+    @givers = people.to_a.dup
   end
 
   def santafy!
     # TODO: correct algorithm
-    @people.find_each do |giver|
-      recipient = pick_recipient(giver)
-      giver.add_recipient(recipient) if recipient
+    @recipients.shuffle.each do |recipient|
+      giver = pick_giver(for: recipient)
+
+      give(from: giver, to: recipient) if giver
     end
   end
 
-  def pick_recipient(giver)
-    @people.where(['id <> ?', giver]).first(order: 'RANDOM()')
+  def pick_giver(options)
+    recipient = options[:for]
+
+    (@givers - [recipient]).sample
+  end
+
+  def give(options)
+    giver = options[:from]
+    recipient = options[:to]
+
+    Gift.create(giver: giver, recipient: recipient)
+    @givers.delete(giver)
+    @recipients.delete(recipient)
   end
 end
