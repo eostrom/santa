5db8f931b9c21eb4309ab5a6df5526d24f50e228
Erik Ostrom <erik@echographia.com>
2013-12-01 14:45:52 -0600

    Assign multiple gifts.

--- app/models/santafier.rb
+++ app/models/santafier.rb
@@ -1,4 +1,6 @@
 class Santafier
+  THRESHOLD = 3
+
   def initialize(people)
     @people = people
 
@@ -7,18 +9,19 @@ class Santafier
   end
 
   def santafy!
-    # TODO: correct algorithm
-    @recipients.shuffle.each do |recipient|
-      giver = pick_giver(for: recipient)
+    until @recipients.length <= 1 # sometimes somebody gets left out :-(
+      @recipients.shuffle.each do |recipient|
+        giver = pick_giver(for: recipient)
 
-      give(from: giver, to: recipient) if giver
+        give(from: giver, to: recipient) if giver
+      end
     end
   end
 
   def pick_giver(options)
     recipient = options[:for]
 
-    (@givers - [recipient]).sample
+    (@givers - [recipient, *recipient.givers.reload]).sample
   end
 
   def give(options)
@@ -26,7 +29,16 @@ class Santafier
     recipient = options[:to]
 
     Gift.create(giver: giver, recipient: recipient)
-    @givers.delete(giver)
-    @recipients.delete(recipient)
+
+    @givers.delete(giver) if done_giving?(giver)
+    @recipients.delete(recipient) if done_receiving?(recipient)
+  end
+
+  def done_giving?(giver)
+    giver.gifts_given.count == THRESHOLD
+  end
+
+  def done_receiving?(recipient)
+    recipient.gifts_received.count == THRESHOLD
   end
 end
