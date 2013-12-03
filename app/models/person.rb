3fddc11506e01eb94df67e7e18880d2afc1b027f
Erik Ostrom <erik@echographia.com>
2013-12-03 14:55:11 -0600

    Delete gifts when their people are deleted.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
-  has_many :gifts_given, class_name: 'Gift', foreign_key: 'giver_id'
-  has_many :gifts_received, class_name: 'Gift', foreign_key: 'recipient_id'
+  has_many :gifts_given, class_name: 'Gift', foreign_key: 'giver_id',
+    dependent: :destroy
+  has_many :gifts_received, class_name: 'Gift', foreign_key: 'recipient_id',
+    dependent: :destroy
   has_many :recipients, class_name: 'Person', through: :gifts_given
   has_many :givers, class_name: 'Person', through: :gifts_received
 
